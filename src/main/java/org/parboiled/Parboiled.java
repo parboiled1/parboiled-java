ba3d261f08fc51db823c8a4fde3e9c9aa0b9cfef
Francis Galiegue <fgaliegue@gmail.com>
2014-04-20 15:26:56 +0200

    Demonstration code using procyon to dump the source of a parser
    However, requires Java 1.7. This will go in a separate package anyway, but at
    least we know it's possible.
    
    Conflicts:
    	src/main/java/org/parboiled/Parboiled.java

--- src/main/java/org/parboiled/Parboiled.java
+++ src/main/java/org/parboiled/Parboiled.java
@@ -16,9 +16,27 @@
 
 package org.parboiled;
 
+import com.strobel.assembler.InputTypeLoader;
+import com.strobel.assembler.metadata.Buffer;
+import com.strobel.assembler.metadata.ClassFileReader;
+import com.strobel.assembler.metadata.ITypeLoader;
+import com.strobel.assembler.metadata.MetadataSystem;
+import com.strobel.assembler.metadata.TypeDefinition;
+import com.strobel.decompiler.DecompilationOptions;
+import com.strobel.decompiler.DecompilerSettings;
+import com.strobel.decompiler.ITextOutput;
+import com.strobel.decompiler.PlainTextOutput;
 import org.parboiled.transform.ParserTransformer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.lang.reflect.Constructor;
+import java.nio.charset.Charset;
 
 import static org.parboiled.common.Preconditions.*;
 import static org.parboiled.common.Utils.*;
@@ -27,6 +45,9 @@ import static org.parboiled.common.Utils.*;
  * Main class providing the high-level entry point into the parboiled library.
  */
 public class Parboiled {
+
+    private static final int CLASSFILEREADER_OPTIONS
+        = ClassFileReader.OPTIONS_DEFAULT | ClassFileReader.OPTION_PROCESS_CODE;
 
     protected Parboiled() {}
 
@@ -83,5 +104,114 @@ public class Parboiled {
         } catch (Exception e) {
             throw new RuntimeException("failed to generate byte code", e);
         }
+    }
+
+    /**
+     * Dump the source of a generated parser class using <a
+     * href="https://bitbucket.org/mstrobel/procyon">procyon</a>
+     *
+     * <p>This will first generate the bytecode of the actual running parser,
+     * then decompile it.</p>
+     *
+     * <p>You have to provide a <em>directory</em> as an argument. This method
+     * will then generate file {@code com/foo/bar/MyParser$$parboiled.java}
+     * under this directory for a parser class whose canonical name is {@code
+     * com.foo.bar.MyParser}.</p>
+     *
+     * @param parserClass the class to dump
+     * @param baseDir the base directory where to write the generated source
+     * @param charset the character coding to use
+     * @param <P> parser class
+     * @param <V> see {@link BaseParser}
+     * @throws RuntimeException {@code baseDir} is not a directory; cannot
+     * create output file or directory; or problem encountered during source
+     * generation.
+     * @throws NullPointerException one argument is null
+     *
+     * @see ParserTransformer#getByteCode(Class)
+     * @see Class#getCanonicalName()
+     */
+    public static <P extends BaseParser<V>, V> void dumpParserSource(
+        final Class<P> parserClass, final File baseDir, final Charset charset)
+    {
+        checkArgNotNull(parserClass, "parser class must not be null");
+        checkArgNotNull(baseDir, "base directory must not be null");
+        checkArgNotNull(charset, "charset must not be null");
+        checkArgument(!baseDir.exists() || baseDir.isDirectory(),
+            "'%s': not a directory", baseDir);
+
+        final String classname = parserClass.getCanonicalName()
+            .replace(".", File.separator) + "$$parboiled.java";
+
+        final File dstfile = new File(baseDir, classname).getAbsoluteFile();
+        final File dir = dstfile.getParentFile();
+
+        checkArgument(dir.mkdirs(), "failed to create directory '%s'", dir);
+
+        OutputStream out = null;
+        OutputStreamWriter writer = null;
+
+        try {
+            out = new FileOutputStream(dstfile);
+            writer = new OutputStreamWriter(out, charset);
+            dumpSource(parserClass, writer);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("failed to open file " + dstfile, e);
+        } catch (Exception e) {
+            throw new RuntimeException("failed to dump source", e);
+        } finally {
+            try {
+                if (writer != null)
+                    writer.close();
+            } catch (IOException ignored) {
+            }
+            try {
+                if (out != null)
+                    out.close();
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    /**
+     * Dump the source of a generated parser class using the system's default
+     * charset
+     *
+     * <p>This simply calls {@link #dumpParserSource(Class, File, Charset)} with
+     * {@link Charset#defaultCharset()} as a charset.</p>
+     *
+     * @param parserClass the class to dump
+     * @param baseDir the base directory where to write the generated source
+     * @param <P> parser class
+     * @param <V> see {@link BaseParser}
+     */
+    public static <P extends BaseParser<V>, V> void dumpParserSource(
+        final Class<P> parserClass, final File baseDir)
+    {
+        dumpParserSource(parserClass, baseDir, Charset.defaultCharset());
+    }
+
+    private static void dumpSource(final Class<?> parserClass,
+        final Writer writer)
+        throws Exception
+    {
+        /*
+         * See https://bitbucket.org/mstrobel/procyon/issue/186
+         */
+        final byte[] content = ParserTransformer.getByteCode(parserClass);
+        final Buffer buf = new Buffer(content);
+
+        final ITypeLoader loader = new InputTypeLoader();
+        final MetadataSystem ms = new MetadataSystem(loader);
+        ms.setEagerMethodLoadingEnabled(true);
+
+        final TypeDefinition type
+            = ClassFileReader.readClass(CLASSFILEREADER_OPTIONS, ms, buf);
+
+        final DecompilationOptions options = new DecompilationOptions();
+        options.setSettings(DecompilerSettings.javaDefaults());
+
+        final ITextOutput out = new PlainTextOutput(writer);
+        options.getSettings().getLanguage().decompileType(type, out, options);
     }
 }
