1bf6744849ed07cd4a884ecc08ec2cc1c823dec2
Francis Galiegue <fgaliegue@gmail.com>
2014-04-22 12:49:52 +0200

    Simplify test code

--- src/test/java/repro/abc/ParserTest.java
+++ src/test/java/repro/abc/ParserTest.java
@@ -7,40 +7,8 @@ public class ParserTest {
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        //repro.abc.Parser parser = Parboiled.createParser(repro.abc.Parser.class);
         repro.abc.Parser parser = Parboiled.createParser(Parser.class);
 
-        new BasicParseRunner(parser.InputLine()).run("");
-//        Boolean done = false;
-//
-//        while (!done) {
-//            System.out.print("test> ");
-//            String input = new Scanner(System.in).nextLine();
-//            if (StringUtils.isEmpty(input)) {
-//                System.out.println("Empty input, exiting.");
-//                done = true;
-//            } else {
-//                ParsingResult<?> result = new RecoveringParseRunner(parser.InputLine()).run(input);
-//
-//                if (result.hasErrors()) {
-//                    System.out.println("\nParse Errors:\n" + printParseErrors(result));
-//                }
-//
-//                Object value = result.parseTreeRoot.getValue();
-//                if (value != null) {
-//                    String str = value.toString();
-//                    int ix = str.indexOf('|');
-//                    if (ix >= 0) str = str.substring(ix + 2); // extract value part of AST node toString()
-//                    System.out.println("Input: " + input + " = " + str + '\n');
-//                }
-//                if (value instanceof GraphNode) {
-//                    Formatter<GraphNode> f = new ToStringFormatter<GraphNode>(null);
-//                    System.out.println("\nAbstract Syntax Tree:\n" +
-//                        GraphUtils.printTree((GraphNode) value, f) + '\n');
-//                } else {
-//                    System.out.println("\nParse Tree:\n" + printNodeTree(result) + '\n');
-//                }
-//            }
-//        }
+        new BasicParseRunner(parser.InputLine());
     }
 }
