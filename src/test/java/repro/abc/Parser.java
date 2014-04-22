df0adf8b96230852fbb64bea06a2f77c82968e3f
Francis Galiegue <fgaliegue@gmail.com>
2014-04-22 13:36:17 +0200

    Parser: creaming out again!
    Not very many rules left, now.

--- src/test/java/repro/abc/Parser.java
+++ src/test/java/repro/abc/Parser.java
@@ -8,26 +8,11 @@ import org.parboiled.annotations.BuildParseTree;
 public class Parser extends BaseParser<Object> {
 
     public Rule InputLine() {
-        return Sequence(R23(), EOI);
-    }
-
-    public Rule R23() {
-        return Sequence(R29(), R26());
-    }
-
-    public Rule R26() {
-        return Sequence(
-            Optional(
-                R52(),
-                R60()
-            ),
-            R67()
-        );
+        return Sequence(R29(), EOI);
     }
 
     public Rule R29() {
         return Sequence(
-            R47(),
             R31(),
             R48()
         );
@@ -60,50 +45,11 @@ public class Parser extends BaseParser<Object> {
     }
 
     public Rule R35() {
-        return R23();
-    }
-
-    public Rule R47() {
-        return Ch('(');
+        return R29();
     }
 
     public Rule R48() {
         return Ch(')');
-    }
-
-    public Rule R52() {
-        return R58("A2");
-    }
-
-    public Rule R58(String string) {
-        return Sequence(IgnoreCase(string), R60());
-    }
-
-    public Rule R60() {
-        return ZeroOrMore(FirstOf(R62(), R61())
-        );
-    }
-
-    public Rule R61() {
-        return FirstOf(Ch(' '), Ch('\t'), Ch('\f'), R63());
-    }
-
-    public Rule R62() {
-        return Sequence("/*", ZeroOrMore(FirstOf(R61(), R67())), "*/");
-    }
-
-    public Rule R63() {
-        return AnyOf("\n\r");
-    }
-
-    public Rule R67() {
-        return FirstOf(
-            CharRange('a', 'z'),
-            CharRange('A', 'Z'),
-            CharRange('0', '9'),
-            Ch('_'),
-            Ch('$')
-        );
     }
 }
 
